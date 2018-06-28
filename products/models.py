4ddb86a86560ae884f259523824ef2b3e83d6d88
Francisco Perez <francofuji@gmail.com>
2018-06-28 02:27:11 -0400

    defining models (work in progress)

--- products/models.py
+++ products/models.py
@@ -2,5 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class Unit(models.Model):
+    name = models.CharField(max_length=10)
+    code = models.CharField(max_field=3)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=50)
+    unit = models.ForeignKey(Unit)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
